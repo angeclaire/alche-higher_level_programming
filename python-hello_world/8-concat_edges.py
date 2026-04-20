07761bf39a5c78107181691a43de69f5ed620f82
angeclaire <c.uwineza3@alustudent.com>
2026-04-20 11:55:33 -0700

    updated

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming with Python"
-print(str[39:66] + str[0:6] + str[8] + str[14:18] + str[66:])
+str = str[39:67] + str [107:112] + str[:6]
+print(str)
