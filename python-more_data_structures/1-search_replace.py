7c3d24ba0fd48ea42b5bd28e9126a383886579a0
angeclaire <c.uwineza3@alustudent.com>
2026-04-24 00:00:12 -0700

    update

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if i == search else i for i in my_list] 
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list 
