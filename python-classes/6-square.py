127ac06c5829c0f4cadcf02f8a7c278b95c13562
angeclaire <c.uwineza3@alustudent.com>
2026-04-29 10:44:00 -0700

    updates

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,27 +47,4 @@ class Square:
         """Set the position of the square.
 
         Args:
-            value (tuple): The new position of the square.
-
-        Raises:
-            TypeError: If value is not a tuple of 2 positive integers.
-        """
-        if (not isinstance(value, tuple) or len(value) != 2 or
-                not isinstance(value[0], int) or not isinstance(value[1], int)
-                or value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        """Calculate and return the current square area."""
-        return self.__size ** 2
-
-    def my_print(self):
-        """Print the square with the # character using position offsets."""
-        if self.__size == 0:
-            print("")
-            return
-        for i in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+            value (tuple): The new position of the
