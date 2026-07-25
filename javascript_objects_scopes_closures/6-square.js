901e20441b415c29a32f48ea1db4f77f09fd86fe
angeclaire <c.uwineza3@alustudent.com>
2026-07-25 10:09:36 -0700

    more update

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Square = require('./5-square');
+
+class MySquare extends Square {
+  charPrint (c) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += char;
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = MySquare;
